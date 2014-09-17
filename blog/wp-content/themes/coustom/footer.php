4f1534c5cd524735ebcdafcefb51fe56ae08085c
nithinmurali <imnmfotmal@gmail.com>
2014-09-17 15:11:10 +0530

    added basic coustom theme

---
+++ blog/wp-content/themes/coustom/footer.php
@@ -0,0 +1,13 @@
+<!-- begin footer.php -->
+
+<div id="footer">
+<?php get_template_part( 'site_files/site_footer' ); ?>
+</div>
+<!-- footer -->
+</div>
+<!-- page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
+<!-- end footer.php -->
---
+++ blog/wp-content/themes/coustom/header.php
@@ -0,0 +1,37 @@
+<!-- begin header.php -->
+
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+<body>
+
+<div id="page">
+  <div class="title block-full color-fff ">
+<?php get_template_part( 'site_files/site_title'); ?>
+  </div>
+  <!-- title -->
+  <div class="nav block-full color-ccc ">
+<?php get_template_part( 'site_files/site_nav'); ?>
+  </div>
+  <!-- nav -->
+
+<!-- end header.php -->
